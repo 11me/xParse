08f08efbd9c2bbc490cf39f09471703cecb07a9f
lime <lime>
2021-12-12 22:20:03 +0300

    add rss/xml headers

--- lib/fetcher.d.ts
+++ lib/fetcher.d.ts
@@ -1,4 +1,6 @@
 import { Fetcher } from "./types";
 export declare class XMLFetcher implements Fetcher {
+    headers: Record<string, string>;
+    constructor();
     fetch(source: string): Promise<string>;
 }
--- src/fetcher.ts
+++ src/fetcher.ts
@@ -3,9 +3,20 @@ import fetch from 'node-fetch-commonjs';
 
 export class XMLFetcher implements Fetcher {
 
+  headers: Record<string, string>
+
+  constructor() {
+    this.headers = {
+      'User-Agent': 'xParse',
+      'Accept': 'application/rss+xml,application/xhtml+xml,application/xml;q=0.9'
+    }
+  }
+
   async fetch(source: string): Promise<string> {
 
-    const resp = await fetch(source)
+    const resp = await fetch(source, {
+      headers: this.headers
+    })
 
     return await resp.text()
 
