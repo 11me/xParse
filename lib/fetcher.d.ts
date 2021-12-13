4b37fb9d189f517f1d91271fd08bf68d3d6afba3
lime <lime>
2021-12-13 21:38:42 +0300

    remove autogenerated lib directory

--- lib/fetcher.d.ts
+++
@@ -1,6 +0,0 @@
-import { Fetcher } from "./types";
-export declare class XMLFetcher implements Fetcher {
-    headers: Record<string, string>;
-    constructor();
-    fetch(source: string): Promise<string>;
-}
