4d0aba71b05cd183af2741b0c83e3e9526278af0
lime <lime>
2021-12-16 21:56:19 +0300

    add hackernews options

--- examples/html_example.js
+++ examples/html_example.js
@@ -1,11 +1,14 @@
 const { HTMLParser } = require('../lib/parsers/html-parser');
 const vc = require('../lib/options/vc-ru');
+const hn = require('../lib/options/hackernews-com');
 
 const htmlParser = new HTMLParser();
 
 (async () => {
 
-  const html = await htmlParser.parse(vc)
+  //const html = await htmlParser.parse(vc)
+  // hackernews parser
+  const html = await htmlParser.parse(hn)
 
   console.log(html);
 
---
+++ src/options/hackernews-com/index.ts
@@ -0,0 +1,22 @@
+module.exports = {
+
+  'description': {
+    'url': 'https://thehackernews.com',
+    'page_selector': 'a.story-link',
+    'type': 'HackerNews'
+  },
+  'options': {
+    'title': {
+      'selectors': ['h1.story-title'],
+    },
+    'author': {
+      'selectors': ['span.author:nth-child(4) > a:nth-child(1)']
+    },
+    'pubDate': {
+      'selectors': ['span.author:nth-child(2)']
+    },
+    'content': {
+      'selectors': ['p']
+    }
+  }
+};
