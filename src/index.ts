9d89042257598e7e796bdf147f11e4c77a6070b2
lime <lime>
2022-01-03 21:34:58 +0300

    export options
    rename: hackernews -> thehackernews

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { RSSParser } from './parsers/rss-parser';
 export { HTMLParser } from './parsers/html-parser';
+export { thehackernews }  from './options/thehackernews-com';
+export { vcru } from './options/vc-ru';
