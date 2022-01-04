c0851c254525e2e8bce39c400c766e19e85e3a60
lime <lime>
2022-01-04 13:29:49 +0300

    braking changes and code refactor

--- src/parsers/rss-parser.ts
+++ src/parsers/rss-parser.ts
@@ -1,23 +1,20 @@
-import { Parser } from '../types';
 import { parseStringPromise } from 'xml2js';
-import { XMLFetcher } from '../fetcher';
 import { Feed, Options } from '../models';
+import { BaseParser } from './base-parser';
 
-export class RSSParser implements Parser {
+export class RSSParser extends BaseParser {
 
-  fetcher: XMLFetcher;
-
-  constructor() {
-    this.fetcher = new XMLFetcher()
+  constructor(fetchProvider?: Function) {
+    super(fetchProvider)
   }
 
   public async parse(options: Options): Promise<Feed[]> {
 
     const from = options['description']['url'];
 
-    const fetchedText = await this.fetcher.fetch(from)
+    const resource = await this.fetch(from)
 
-    const result = await parseStringPromise(fetchedText);
+    const result = await parseStringPromise(resource);
 
     return result.rss.channel;
 
