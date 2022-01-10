6548e9ce6485c01d2141639099645c3ec605bc53
lime <lime>
2022-01-11 00:31:32 +0300

    make creator required

--- src/models/feed.ts
+++ src/models/feed.ts
@@ -2,7 +2,7 @@ export interface Feed {
   title:        string
   link?:        string
   author?:      string // TODO: remove author and use creator instead
-  creator?:     string
+  creator:      string
   pubDate:      string
   content:      string // TODO: remove content and use description instead
   description?: string
